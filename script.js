84d0683f87a92c5fabf4fbf6236984740efd5791
Ahmed Wagdi <ahmed.w.amin@gmail.com>
2023-12-16 12:59:46 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,154 @@
+//Int Variables
+var subject_1 = 15;
+var subject_2 = 10;
+var subject_3 = 6;
+//Calculating results
+var fullResult = subject_1 + subject_2 + subject_3;
+//String variables
+var studentName = "Zain Ahmed Abdelhammed";
+
+//Object Variable
+var marks = {
+    "maths": 15,
+    "since": 10,
+    "arabic": 6,
+    "english": 8,
+}
+
+var mathsValue = marks["english"]
+var mathAndArabic = marks["arabic"] + marks["maths"]
+console.log("student " + studentName + " " + mathAndArabic)
+
+//Arrays variable
+
+var marksList = [
+    15, 10, 6, 8
+]
+var subjects = [
+    "maths",
+    "since",
+    "arabic",
+    "english",
+]
+
+
+//hybrid  variable 
+var marksAndAges = [
+    { "name": "Zain", "age": 24, "marks": 15 },
+    { "name": "Mohamed", "age": 22, "marks": 10 },
+    { "name": "Hassan", "age": 21, "marks": 6 },
+]
+
+for (var item of marksAndAges) {
+    console.log(item['name'] + " age is : " + item['age'])
+}
+
+
+//Dummy json response 
+
+var product = {
+    "id": 1,
+    "title": "iPhone 9",
+    "description": "An apple mobile which is nothing like apple",
+    "price": 549,
+    "discountPercentage": 12.96,
+    "rating": 4.69,
+    "stock": 94,
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    "images": [
+        "https://i.dummyjson.com/data/products/1/1.jpg",
+        "https://i.dummyjson.com/data/products/1/2.jpg",
+        "https://i.dummyjson.com/data/products/1/3.jpg",
+        "https://i.dummyjson.com/data/products/1/4.jpg",
+        "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+    ]
+}
+
+console.log(product['title'])
+console.log(product['description'])
+
+
+var products = [
+    {
+        "id": 1,
+        "title": "iPhone 9",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    }, {
+        "id": 1,
+        "title": "iPhone 10",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    }, {
+        "id": 1,
+        "title": "iPhone 11",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    }, {
+        "id": 1,
+        "title": "iPhone 12",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    }
+]
+
+// console.log(products[0]['title'])
+
+
+for (var item of products){
+    console.log(item['title'])
+}
